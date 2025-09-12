2b8b47296a3d930b86faa43cce868e6fe0d447ab
NicollyAAguiar <nicollyaguiar2009@gmail.com>
2025-09-12 10:20:49 -0300

    quarto commit

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,19 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View, TextInput } from 'react-native';
 import { styles } from './style';
-import { useState } from 'react';
+
 
 export function SignInin() {
-
-  const [text,setText] = useState("");
  
   return (
     <View style={styles.container}>
-      <Text>oi, digita algo ai :3</Text>
-      <TextInput style={{width:200,height:50,borderWidth:3}} onChangeText={setText} />
-        <Text>você digitou: {text} </Text>
-        
-      <StatusBar style="auto" />
+
     </View>
   );
 }
